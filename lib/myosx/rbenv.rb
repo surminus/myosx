c8b063d102a0ccfe307b8c0dcea957adeed3646f
Laura Martin <surminus@gmail.com>
2017-02-04 17:07:36 +0000

    Add rbenv class
    This just manages ruby versions for the time-being.
    
    For future functionality I'd like to also manage aliases.

--- lib/myosx/rbenv.rb
+++ lib/myosx/rbenv.rb
@@ -1,2 +1,38 @@
 #!/usr/bin/env ruby
-# Configures rbenv
+#
+# Class which installs rbenv packages, and installs ruby versions
+#
+# This class does not add the shims path to your shell or manage any aliases
+#
+require 'yaml'
+require_relative 'core'
+
+class Rbenv
+  def initialize
+    Core.new
+    @config = $global_config['rbenv']
+  end
+
+  def install
+    packages = ['rbenv', 'rbenv-aliases', 'rbenv-bundler']
+    packages.each do |pkg|
+      unless system("brew list #{pkg} >/dev/null 2>&1")
+        puts "Installing #{pkg}"
+        system("brew install #{pkg}")
+      end
+    end
+
+    versions = @config['versions']
+    puts "Checking Ruby version:"
+    versions.each do |version|
+      puts "  #{version}"
+      system("rbenv install #{version} --skip-existing")
+    end
+  end
+
+  def exec
+    if @config['manage']
+      install
+    end
+  end
+end
