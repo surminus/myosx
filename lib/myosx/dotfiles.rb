ebb27c228da2a518f7717d629d90defdf876f3dc
Laura Martin <surminus@gmail.com>
2017-03-17 20:44:13 +0000

    Update dotfile methods slightly

--- lib/myosx/dotfiles.rb
+++ lib/myosx/dotfiles.rb
@@ -15,44 +15,39 @@ class Dotfiles
     File.join($workspace, 'dotfiles')
   end
 
-  def repo
-    dotfile_repo = @config['repo']
-    if Git.ls_remote(dotfile_repo)
+  def repo(repo)
+    if Git.ls_remote(repo)
       unless File.exist?(dotfiledir)
         puts "Cloning #{dotfile_repo}"
-        Git.clone(dotfile_repo, 'dotfiles', :path => $workspace)
+        Git.clone(repo, 'dotfiles', :path => $workspace)
       else
         g = Git.init(dotfiledir)
-        puts "Pulling latest #{dotfile_repo}"
+        puts "Pulling latest #{repo}"
         g.pull
       end
     end
   end
 
+  def link(file, dest)
+    source_file = "#{dotfiledir}/#{file}"
 
-  def link
-    dotfiles = @config['files']
+    dest = File.expand_path(dest)
+    raise "#{source_file} doesn't exist! Check your config or repo" unless File.exist?(source_file)
 
-      dotfiles.each do |file|
-      source_file = "#{dotfiledir}/#{file}"
-
-      raise "#{source_file} doesn't exist! Check your config or repo" unless File.exist?(source_file)
-
-      link_file = File.join(Dir.home, file.prepend('.'))
-
-      if File.exist?(link_file)
-        FileUtils.remove_file(link_file)
-      end
-
-      puts "Linking #{source_file} to #{link_file}"
-      FileUtils.ln_sf(source_file, link_file)
+    if File.exist?(dest)
+      FileUtils.remove_file(dest)
     end
+
+    puts "Linking #{source_file} to #{dest}"
+    FileUtils.ln_sf(source_file, dest)
   end
 
   def exec
     if @config['manage']
-      if repo
-        link
+      if repo(@config['repo'])
+        @config['files'].each do |k, v|
+          link(k, v)
+        end
       end
     end
   end
