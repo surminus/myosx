22f4814faad0912c4f6d8e34cc5f13d90df3a169
Laura Martin <surminus@gmail.com>
2017-03-24 14:11:18 +0000

    Revamp homebrew method
    Revamp the method so it can be tested.
    
    Do not use the "manage" config. If it isn't in the config, we shouldn't
    do anything.

--- lib/myosx/homebrew.rb
+++ lib/myosx/homebrew.rb
@@ -10,30 +10,40 @@ class Homebrew < Config
 
   def packages
     @config = $global_config['homebrew']
+    return @config['packages']
+  end
+
+  def brewfile
+    return File.join($workspace, 'Brewfile')
+  end
+
+  def foo(bar)
+    puts bar
+  end
+
+  def brewfile_out(packages)
+    output = ''
+    packages.each do |type, package_array|
+      package_array.each do |package|
+        output << "#{type} #{package}\n"
+      end
+    end
+    return output
   end
 
   def bundle
     system("brew tap Homebrew/bundle")
     puts "Updating Brewfile"
-    packages = @config['packages']
-    brew_packages = packages['brew']
-    tap_packages = packages['tap']
-    cask_packages = packages['cask']
 
-    File.open(File.join($workspace, 'Brewfile'), "w+") do |f|
-      brew_packages.each { |p| f.puts "brew '#{p}'" }
-      tap_packages.each { |p| f.puts "tap '#{p}'" }
-      cask_packages.each { |p| f.puts "cask '#{p}'" }
-    end
-    brewfile = File.join($workspace, 'Brewfile')
+    File.open(brewfile, "w+") {
+      |file| file.write(brewfile_out(packages))
+    }
 
     puts "Installing packages from #{brewfile}"
     system("brew bundle --file=#{brewfile}")
   end
 
   def exec
-    if @config['manage']
-      bundle
-    end
+    bundle
   end
 end
