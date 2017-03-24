ff6b80bada3776a76fcd2dcc9a9e3744830912bd
Laura Martin <surminus@gmail.com>
2017-03-24 14:12:12 +0000

    Update tests

---
+++ spec/homebrew_spec.rb
@@ -0,0 +1,13 @@
+#require "spec_helper"
+require_relative "../lib/myosx/homebrew"
+
+global_config = YAML.load_file('spec/example.config.yaml')
+config = global_config['homebrew']
+packages = config['packages']
+
+describe "homebrew" do
+  it "takes a list of packages and returns in Brewfile format" do
+    expect(Homebrew.new.brewfile_out(packages)).to eq("brew jimi\nbrew hendrix\ncask eric\ncask clapton\ntap bob\ntap marley\n")
+  end
+
+end
