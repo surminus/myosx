c756a32d2d6e06753fa90e0491d1427119571dfd
Laura Martin <surminus@gmail.com>
2017-04-02 16:22:45 +0100

    add-simplecov

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 
 gem 'git'
 
+gem 'simplecov', :require => false, :group => :test
+
 group :test do
   gem "rake"
   gem "rspec"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,7 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require 'simplecov'
+SimpleCov.start
+
 require "myosx"
 
 Dir[File.dirname(__FILE__) + '/lib/myosx/*.rb'].each {|file| require file }
